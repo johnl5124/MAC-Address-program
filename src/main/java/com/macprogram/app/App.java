1df6954502c9db45a29f11c0ad9724d9d26aa388
John Lloyd <johnl5124@gmail.com>
2022-07-05 13:17:11 +0100

    Initial Commit

---
+++ src/main/java/com/macprogram/app/App.java
@@ -0,0 +1,9 @@
+package com.macprogram.app;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/macprogram/mac_prog.java
@@ -0,0 +1,9 @@
+package com.macprogram;
+
+public class mac_prog 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome... starting GUI");
+    }    
+}
